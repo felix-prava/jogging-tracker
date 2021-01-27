41b4461f535ee306801705c77f2aa0bbedd3bcff
felix24mihai <mihai_prava@yahoo.com>
2021-01-27 02:24:09 +0200

    Style app with Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
-  get 'static_pages/help'
+  get 'contact' => 'static_pages#contact'
   root 'static_pages#home'
   resources :users
 end
